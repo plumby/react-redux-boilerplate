dcc69baacf342d6ee900d1704231cc050a823d0f
Ian Plumb <ian@Ians-MacBook-Pro.local>
2016-10-21 18:55:44 +0100

    added a few missing imports for dev tools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
 import rootReducer from 'reducers';
 
 export default function configureStore(initialState) {
-  return createStore(
-    rootReducer,
-    initialState
-  );
+  /* eslint-disable no-underscore-dangle */
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  /* eslint-enable */
+  const store = createStore(rootReducer, initialState, composeEnhancers(
+    applyMiddleware(thunk)
+  ));
+
+  return store;
 }
